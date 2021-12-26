846fa2f90f63fb94320e76fd3bf196c8b4ce3b7f
Коюшев Павел <pkoyushev@gmail.com>
2021-12-26 16:05:56 +0300

    Initial Commit

---
+++ SwiftUI-Helper/SwiftUI_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HelperApp.swift
+//  SwiftUI-Helper
+//
+//  Created by Pavel Koyushev on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
