c68d0a6c82c2f5cddb489e172e9b2c96eded99a7
Pavel Koyushev <pkoyushev@gmail.com>
2023-06-17 20:23:43 +0300

    new scrollview

---
+++ SwiftUI-Helper/Modules/CardsView.swift
@@ -0,0 +1,18 @@
+//
+//  CardsView.swift
+//  SwiftUI-Helper
+//
+//  Created by Pavel Koyushev on 17.06.2023.
+//
+
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardsView()
+}
